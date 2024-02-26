f0f95caa023188f7a34370e4b8bacb43ead03254
Prijith Pradeep <prijith.babu-pradeep18@imperial.ac.uk>
2024-02-26 14:33:26 +0000

    Updated yaml scripts in tests for Scouting

--- tests/runme_dqcd_newmodels_qcd_deploy.sh
+++ tests/runme_dqcd_newmodels_qcd_deploy.sh
@@ -8,13 +8,13 @@
 source $HOME/setconda.sh
 conda activate icenet
 
-ICEPATH="/vols/cms/mmieskol/icenet"
+ICEPATH="/vols/cms/pb4918/icenet_v3/icenet"
 cd $ICEPATH
 echo "$(pwd)"
 source $ICEPATH/setenv.sh
 
-CONFIG="tune0_new.yml"
-DATAPATH="/vols/cms/khl216"
-CONDITIONAL=1
+CONFIG="tune0_new_scouting.yml"
+DATAPATH="/vols/cms/pb4918"
+CONDITIONAL=0
 
 python analysis/dqcd_deploy.py --runmode deploy --use_conditional $CONDITIONAL --inputmap 'include/QCD_newmodels_deploy.yml' --modeltag scenarioA_all --grid_id $GRID_ID --grid_nodes $GRID_NODES --config $CONFIG --datapath $DATAPATH
--- tests/runme_dqcd_newmodels_signal_deploy.sh
+++ tests/runme_dqcd_newmodels_signal_deploy.sh
@@ -8,13 +8,14 @@
 source $HOME/setconda.sh
 conda activate icenet
 
-ICEPATH="/vols/cms/mmieskol/icenet"
+ICEPATH="/vols/cms/pb4918/icenet_v3/icenet"
 cd $ICEPATH
 echo "$(pwd)"
 source $ICEPATH/setenv.sh
 
-CONFIG="tune0_new.yml"
-DATAPATH="/vols/cms/khl216"
-CONDITIONAL=1
+CONFIG="tune0_new_scouting.yml"
+DATAPATH="/vols/cms/pb4918"
+CONDITIONAL=0
 
+#Change conditional to 1 if training was conditional
 python analysis/dqcd_deploy.py --runmode deploy --use_conditional $CONDITIONAL --inputmap 'include/scenarioA_deploy.yml' --modeltag scenarioA_all --grid_id $GRID_ID --grid_nodes $GRID_NODES --config $CONFIG --datapath $DATAPATH
