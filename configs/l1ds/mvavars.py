9b2afcd8a92746a6234a791ad30fefca5197ee76
Prijith Pradeep <prijith.babu-pradeep18@imperial.ac.uk>
2024-11-28 16:40:07 +0000

    Updated to plot MVA vs var correlations

--- configs/l1ds/mvavars.py
+++ configs/l1ds/mvavars.py
@@ -46,7 +46,15 @@ LOAD_VARS += MVA_SCALAR_VARS
 LOAD_VARS += MVA_JAGGED_VARS
 
 #Mutual information regularization targets
+#MI_VARS = [
+#    'dijet_m'
+#]
+
+#MI_VARS = [
+#    'dijet_deta'
+#]
+
 MI_VARS = [
-    #'dijet_deta',
+    'dijet_deta',
     'dijet_m'
 ]
